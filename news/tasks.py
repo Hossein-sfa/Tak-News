7aa76a4e6e00704681556a187dfd6c7f3f5967c7
Hossein Safaei <hossein@Hosseins-MacBook.local>
2024-08-12 20:01:22 +0330

    add comments

--- news/tasks.py
+++ news/tasks.py
@@ -6,45 +6,74 @@ import requests
 from .models import News, Tag
 
 
+# Function to crawl news articles
 def crawl_news():
     added_news = 0
     page_number = 1
     news_archive_url = 'https://www.zoomit.ir/archive/?sort=Newest&publishPeriod=All&readingTimeRange=All&pageNumber='
     driver = webdriver.Chrome()
+    
+    # Loop through pages (up to 500)
     while page_number <= 500:
         try:
             driver.get(news_archive_url + str(page_number))
-            sleep(5)
+            sleep(5)  # Wait for page to load and avoid throttling
+            
+            # Parse the page source
             beautiful_soup = bs(driver.page_source, 'html.parser')
+            
+            # Find all news article links
             news_links_tags = beautiful_soup.findAll('a', attrs={
                 'class': 'link__CustomNextLink-sc-1r7l32j-0 eoKbWT BrowseArticleListItemDesktop__WrapperLink-zb6c6m-6 bzMtyO'})
+            
             page_number += 1
+            
+            # Process each news article
             for link_tag in news_links_tags:
                 link = link_tag['href']
+                
+                # Check if the news article already exists in the database
                 if not News.objects.filter(source=link).exists():
+                    # Fetch and parse the article page
                     soup = bs(requests.get(link).content, 'html.parser')
+                    
+                    # Extract title
                     header = soup.find('h1',
                                        attrs={'class': 'typography__StyledDynamicTypographyComponent-t787b7-0 fzMmhL'}) \
                              or soup.find('h1',
                                           attrs={
                                               'class': 'typography__StyledDynamicTypographyComponent-t787b7-0 jQMKGt'})
                     title = header.get_text()
+                    
+                    # Extract tags
                     tags_elements = soup.find('div', attrs={'class': 'flex__Flex-le1v16-0 kDyGrB'})
                     a_tags = tags_elements.findAll('a')
+                    
+                    # Extract content
                     paragraphs_tags = soup.findAll('p', attrs={
                         'class': 'typography__StyledDynamicTypographyComponent-t787b7-0 fZZfUi ParagraphElement__ParagraphBase-sc-1soo3i3-0 gOVZGU'})
                     paragraphs = [p.get_text() for p in paragraphs_tags]
                     content = '\n'.join(paragraphs)
+                    
+                    # Create new News object
                     crawled_news = News.objects.create(title=title, content=content, source=link)
+                    
+                    # Create or get Tag objects and add them to the News object
                     tags_objects = [Tag.objects.get_or_create(name=tag.get_text()) for tag in a_tags]
                     crawled_news.tags.add(*tags_objects)
                     crawled_news.save()
+                    
                     added_news += 1
                 else:
+                    # If the article already exists, stop crawling because rest are already crawled
                     print('crawled news: ' + str(added_news))
                     driver.quit()
                     return
         except Exception as e:
             print(e)
+    
+    # Print the total number of crawled news articles
     print('crawled news: ' + str(added_news))
+    
+    # Close the WebDriver
     driver.quit()
